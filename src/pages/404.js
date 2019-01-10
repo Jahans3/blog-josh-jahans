58414adfaac6e109c466f50ab6e61664ca683998
Joshua Jahans <joshua.jahans@trinitymirror.com>
2019-01-10 15:20:15 +0000

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,13 +3,15 @@ import React from 'react'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
 
-export default function NotFoundPage ({ data, location }) {
-  const siteTitle = data.site.siteMetadata.title;
+export default function NotFoundPage ({
+  data: { site: { siteMetadata: { title: siteTitle } } } ,
+  location
+}) {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="404: Not Found" />
-      <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>Oopsie doopise</h1>
+      <p>That's a 404: not found.</p>
     </Layout>
   );
 }
